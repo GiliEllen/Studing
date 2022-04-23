45cbda130fd5d51c8533a7bf1e379295ea77458e
GiliEllen <gilimena95@gmail.com>
2022-04-23 11:51:36 +0300

    challenge 4

--- Coding-Challanges/challange4/dist/app.js
+++ Coding-Challanges/challange4/dist/app.js
@@ -1,36 +1,20 @@
 console.log("Connected!");
-// There are two gymnastics teams, Dolphins and Koalas. They compete against each 
-// other 3 times. The winner with the highest average score wins a trophy!
+// Steven wants to build a very simple tip calculator for whenever he goes eating in a 
+// restaurant. In his country, it's usual to tip 15% if the bill value is between 50 and 
+// 300. If the value is different, the tip is 20%.
 // Your tasks:
-// 1. Calculate the average score for each team, using the test data below
-// 2. Compare the team's average scores to determine the winner of the competition, 
-// and print it to the console. Don't forget that there can be a draw, so test for that 
-// as well (draw means they have the same average score)
-// 3. Bonus 1: Include a requirement for a minimum score of 100. With this rule, a 
-// team only wins if it has a higher score than the other team, and the same time a 
-// score of at least 100 points. Hint: Use a logical operator to test for minimum 
-// score, as well as multiple else-if blocks �
-// 4. Bonus 2: Minimum score also applies to a draw! So a draw only happens when 
-// both teams have the same score and both have a score greater or equal 100 
-// points. Otherwise, no team wins the trophy
+// 1. Calculate the tip, depending on the bill value. Create a variable called 'tip' for 
+// this. It's not allowed to use an if/else statement � (If it's easier for you, you can 
+// start with an if/else statement, and then try to convert it to a ternary 
+// operator!)
+// 2. Print a string to the console containing the bill value, the tip, and the final value 
+// (bill + tip). Example: “The bill was 275, the tip was 41.25, and the total value 
+// 316.25”
 // Test data:
-// § Data 1: Dolphins score 96, 108 and 89. Koalas score 88, 91 and 110
-// § Data Bonus 1: Dolphins score 97, 112 and 101. Koalas score 109, 95 and 123
-// § Data Bonus 2: Dolphins score 97, 112 and 101. Koalas score 109, 95 and 106
-var dolphinsScore1 = 96;
-var dolphinsScore2 = 108;
-var dolphinsScore3 = 89;
-var koalasScore1 = 88;
-var koalasScore2 = 91;
-var koalasScore3 = 110;
-var dolphinsAverageScore = (dolphinsScore1 + dolphinsScore2 + dolphinsScore3) / 3;
-var koalasAverageScore = (koalasScore1 + koalasScore2 + koalasScore3) / 3;
-if (dolphinsAverageScore === koalasAverageScore) {
-    console.log("There is a draw!");
-}
-else if (dolphinsAverageScore > koalasAverageScore) {
-    console.log("Dolphins are the winners!");
-}
-else if (dolphinsAverageScore < koalasAverageScore) {
-    console.log("koalas are the winners!");
-}
+// § Data 1: Test for bill values 275, 40 and 430
+// Hints:
+// § To calculate 20% of a value, simply multiply it by 20/100 = 0.2
+// § Value X is between 50 and 300, if it's >= 50 && <= 300 
+var bill = 430;
+var tip = bill <= 300 && bill >= 50 ? bill * 15 / 100 : bill * 20 / 100;
+console.log("The bill is " + bill + " and you should tip " + tip);
